dc34fdce14d748665b23d9af80c0e0741134906e
Shiyan Su <shiyansu@ShiyandeiMac.hub>
2021-10-15 03:01:38 +1000

    user image upload

--- php/src/myProj/application/controllers/User_profile.php
+++ php/src/myProj/application/controllers/User_profile.php
@@ -63,7 +63,7 @@ class user_profile extends CI_Controller {
 
         $this->load->model('user_data');
         $config = array(
-            'upload_path' => "./uploads/",
+            'upload_path' => "./php/src/myProj/uploads/",
             'allowed_types' => "jpg|png|jpeg",
             //'overwrite' => TRUE,
             'max_size' => "2048000", // Can be set to particular file size , here it is 2 MB(2048 Kb)
--- php/src/myProj/application/views/profile_edit_new.php
+++ php/src/myProj/application/views/profile_edit_new.php
@@ -32,7 +32,7 @@
 
                 <button class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" type="submit">Save</button>
                 <div class="text-center">
-                  <a class="small" href="<?php echo base_url(); ?>/index.php/user_profile">Do not want to change? Go back to your profile.</a>
+                  <a class="small" href="<?php echo base_url(); ?>index.php/user_profile">Do not want to change? Go back to your profile.</a>
                 </div>
               </form>
 
